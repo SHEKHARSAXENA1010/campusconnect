b2fb817745ca79875a195b7cc2f6d350e1e7cc66
SHEKHARSAXENA1010 <108870086+SHEKHARSAXENA1010@users.noreply.github.com>
2023-05-25 13:21:54 +0530

    start

---
+++ src/test/java/com/erp/campusconnect/CampusconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erp.campusconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
